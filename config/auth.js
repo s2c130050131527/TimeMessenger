6710da6bd675c7df108b40af7204ea8d41b8f5d6
shrey Naygandhi <shrey.naygandhi@innoplexus.com>
2018-07-27 12:36:31 +0530

    Added Passport for Authentication

---
+++ config/auth.js
@@ -0,0 +1,9 @@
+const auth = {
+  googleAuth: {
+    clientID: 'your-secret-clientID-here',
+    clientSecret: 'your-client-secret-here',
+    callbackURL: 'http://localhost:8080/auth/google/callback',
+  },
+};
+
+export default auth;
