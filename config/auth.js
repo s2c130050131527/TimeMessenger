5f4ee5673935300ddc1e055517893349a1ddd2b0
s2c130050131527 <shreynaygandhi@gmail.com>
2018-07-27 13:25:02 +0530

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,8 +1,8 @@
 const auth = {
   googleAuth: {
     clientID:
-    '177354867713-4qq1rp02k0o66k9ek814jp9b9bpg96cc.apps.googleusercontent.com',
-    clientSecret: 'lACPGgJ36rLCgvDhqLSI0uYd',
+    'CLient-ID',
+    clientSecret: 'client-secret',
     callbackURL: 'http://localhost:8080/auth/google/callback',
   },
 };
