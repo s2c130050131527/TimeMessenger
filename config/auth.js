7320146899eedea4ce8b95b8112b6ca13ae7ffe2
shrey Naygandhi <shrey.naygandhi@innoplexus.com>
2018-07-27 15:09:02 +0530

    Added Placeholder auth

---
+++ config/auth.js
@@ -0,0 +1,10 @@
+const auth = {
+  googleAuth: {
+    clientID:
+    'client-id',
+    clientSecret: 'client-secre',
+    callbackURL: 'http://localhost:3000/auth/google/callback',
+  },
+};
+
+export default auth;
