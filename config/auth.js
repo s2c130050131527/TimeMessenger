56fb4866cad85b08c71e2a1b42aba6144b3ff019
shrey Naygandhi <shrey.naygandhi@innoplexus.com>
2018-07-27 13:21:11 +0530

    Cant understand whats Happening

--- config/auth.js
+++
@@ -1,9 +0,0 @@
-const auth = {
-  googleAuth: {
-    clientID: 'client-id',
-    clientSecret: 'client-secret',
-    callbackURL: 'http://localhost:8080/auth/google/callback',
-  },
-};
-
-export default auth;
