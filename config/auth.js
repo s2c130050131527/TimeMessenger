acfa6d93bfff66249c9fa9217924f832218d2107
shrey Naygandhi <shrey.naygandhi@innoplexus.com>
2018-07-27 14:44:48 +0530

    Init Commit With Auth using Passport

---
+++ config/auth.js
@@ -0,0 +1,10 @@
+const auth = {
+  googleAuth: {
+    clientID:
+    'client-id',
+    clientSecret: 'client-secret',
+    callbackURL: 'http://localhost:3000/auth/google/callback',
+  },
+};
+
+export default auth;
