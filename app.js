0b05bf724f6ee4be188b6c11cfeb1c276287447e
shrey Naygandhi <shrey.naygandhi@innoplexus.com>
2018-07-27 14:31:31 +0530

    Added Authentication for google

--- app.js
+++ app.js
@@ -3,25 +3,38 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
 import db from './database';
-import routes from './components/routes';
+import passportConfig from './config/passport';
+import initRoute from './components/routes';
+import flash from 'connect-flash';
+import session from 'express-session';
 
 var app = express();
-
+passportConfig(passport);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+app.use(session({
+  secret: 'ilovescotchscotchyscotchscotch', // session secret
+  resave: true,
+  saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+initRoute(app, passport);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+  console.log(req.originalUrl);
   next(createError(404));
 });
 
--- config/auth.js
+++ config/auth.js
@@ -1,9 +1,9 @@
 const auth = {
   googleAuth: {
     clientID:
-    'CLient-ID',
-    clientSecret: 'client-secret',
-    callbackURL: 'http://localhost:8080/auth/google/callback',
+    '177354867713-4qq1rp02k0o66k9ek814jp9b9bpg96cc.apps.googleusercontent.com',
+    clientSecret: 'lACPGgJ36rLCgvDhqLSI0uYd',
+    callbackURL: 'http://localhost:3000/auth/google/callback',
   },
 };
 
